839764b6ef9b871e16d22bb6c9c9c0f2750b651d
Soloquie <cesarcamilotulcanerira@gmail.com>
2026-02-23 16:29:54 -0500

    F commit

---
+++ src/test/java/com/uniquindio/CINEMAX/CinemaxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniquindio.CINEMAX;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
